9414e4652183406bb5861fde30d2ec98e63d5da8
changhoji <onkkno1@gmail.com>
2023-04-23 23:57:45 +0900

    Update

--- xv6-public/param.h
+++ xv6-public/param.h
@@ -11,5 +11,4 @@
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
-#define QUEUESIZE    64
 #define PASSWORD     2021035487 //password for scheduler lock and unlock
