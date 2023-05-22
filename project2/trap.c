dd1e7145fa8d5c8075e0e8879c793982022284a8
2021035487 <changholee@hanyang.ac.kr>
2023-05-22 15:05:55 +0900

    Remove: implement of thread system calls

--- project2/trap.c
+++ project2/trap.c
@@ -89,7 +89,7 @@ trap(struct trapframe *tf)
     // In user space, assume process misbehaved.
     cprintf("pid %d tid %d %s: trap %d err %d on cpu %d "
             "eip 0x%x addr 0x%x--kill proc\n",
-            myproc()->pid, myproc()->tid, myproc()->name, tf->trapno,
+            myproc()->pid, myproc()->name, tf->trapno,
             tf->err, cpuid(), tf->eip, rcr2());
     myproc()->killed = 1;
   }
