d7628bb09288d56ce5d85bc2518502b9f27a8594
changhoji <onkkno1@gmail.com>
2023-06-16 19:05:11 +0900

    Rename: double.c -> test.c

--- project3/test.c
+++ project3/test.c
@@ -1,12 +1,14 @@
 #include "types.h"
 #include "user.h"
+#include "stat.h"
 
 #define BUFFERSIZE 1024
 
 int
 main(int argc, char *argv[])
 {
-  int datasize = 1*1024*1024;
+  int datasize = 16*1024*1024;
+  int flag = 0;
   uint written = 0;
   char buf[BUFFERSIZE];
   int fd = open("double.txt", 0x200 | 0x002);
@@ -22,12 +24,18 @@ main(int argc, char *argv[])
     uint temp = write(fd, buf, BUFFERSIZE);
     if(temp == -1){
       printf(2, "failed to write file\n");
+      flag = 1;
       break;
     }
     written += temp;
     cnt++;
     if(cnt % 50 == 0)
       printf(1, "write success: + %d.. => %d\n", temp, written);
+  }
+
+  if(flag){
+    printf(2, "test failed\n");
+    exit();
   }
 
   printf(1, "written = %d bytes | taken time: %d ticks\n", written, uptime()-time);
